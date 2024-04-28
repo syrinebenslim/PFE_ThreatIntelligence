e2c930518488e6cb9ae6fa1e7500fd0d7e231e3d
Mohamed ZOUAOUI <mzouaoui@solocal.com>
2024-04-28 22:41:52 +0200

    Add PyMYSQL Client

--- watcher/schemas/ingest_entry_point.py
+++ watcher/schemas/ingest_entry_point.py
@@ -3,11 +3,18 @@
 
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from watcher.schemas.ingest_tidb import QueryPlayer
+from watcher.schemas.ingest_tidb import QueryPlayer, DatabaseConnection
 
 
 def main():
     # Use a breakpoint in the code line below to debug your script.
+    try:
+        engine = DatabaseConnection().engine
+        print(
+            f"Connection to the {engine} for user {engine} created successfully.")
+    except Exception as ex:
+        print("Connection could not be made due to the following error: \n", ex)
+
 
     QueryPlayer().simple_example()
     QueryPlayer().trade_example()
