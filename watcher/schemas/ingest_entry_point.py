1ccbe6c91078ccd9198828ed620f4165ced0004d
Mohamed ZOUAOUI <mzouaoui@solocal.com>
2024-04-28 23:52:29 +0200

    Add PyMYSQL Client

--- watcher/schemas/ingest_entry_point.py
+++ watcher/schemas/ingest_entry_point.py
@@ -1,6 +1,8 @@
 # This is a sample Python script.
 
 
+from sqlalchemy.orm import declarative_base
+
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from watcher.schemas.ingest_tidb import QueryPlayer, DatabaseConnection
@@ -9,13 +11,17 @@ from watcher.schemas.ingest_tidb import QueryPlayer, DatabaseConnection
 def main():
     # Use a breakpoint in the code line below to debug your script.
     session = None
+    Base = declarative_base()
     try:
         engine = DatabaseConnection().engine
+
+        Base.metadata.create_all(engine)
 
         print(
             f"Connection to the {engine} for user {engine} created successfully.")
         Session = DatabaseConnection().get_sessions()
         session = Session()
+
     except Exception as ex:
         print("Connection/Session could not be made due to the following error: \n", ex)
 
